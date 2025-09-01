e3b4b601532afea658cad73e7c3634f86f516700
jagadeepanm2422k1442-sketch <jagadeepanm2422k1442@gmail.com>
2025-09-01 19:01:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling for nav links
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  });
-});
